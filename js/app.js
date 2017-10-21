ea6203a8728826aa7905a660512271256c183905
ElaSujeta <ela.sujeta@o2pl>
2017-10-21 15:59:23 +0200

    teoria z javascript

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log("działa!");
+var myName="Ela";
+console.log(myName);
+
+var friends =['Agata', 'Ela', 'Ewa', 'Natalia'];
+
+console.log(friends[1]);
+
+var a = 5;
+var b = 12;
+if (a > b) {
+    console.log("hurra!");
+} else{
+    console.log("pierwsza mniejsza od drugiej");
+}
+
+for(var i=0; i <= friends.length-1; i++) {
+    console.log(friends[i]);
+}
